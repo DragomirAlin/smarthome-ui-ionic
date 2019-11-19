4646a22307471f0b4f0980b8c37a78171279d8ff
DragomirAlin <44725219+DragomirAlin@users.noreply.github.com>
2019-11-19 08:51:28 +0200

    delete tabs

--- SHome/src/app/app-routing.module.ts
+++ SHome/src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  { path: 'welcome', loadChildren: './welcome/welcome.module#WelcomePageModule' },
+ 
+  { path: 'welcome', loadChildren: './welcome/welcome.module#WelcomePageModule' },
   { path: 'administrator', loadChildren: './administrator/administrator.module#AdministratorPageModule' },
   { path: 'manager', loadChildren: './manager/manager.module#ManagerPageModule' }
 
